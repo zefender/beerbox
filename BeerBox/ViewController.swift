2a9849b65e0270efa89dd64fe65ffc3bde9825df
Кузяев Максим <mkuzyaev@avito.ru>
2016-02-14 19:13:26 +0300

    Initial Commit

---
+++ BeerBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeerBox
+//
+//  Created by Кузяев Максим on 14.02.16.
+//  Copyright © 2016 zefender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
